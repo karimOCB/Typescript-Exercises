ebc8c5657f7441c579d5787fd0ef3e380ced4df3
Chaya <chaya.karim.omar@gmail.com>
2024-11-20 17:52:42 -0300

    0.01

---
+++ 100exercises.ts
@@ -0,0 +1,20 @@
+// 1
+
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+] as const;
+
+type Color = (typeof COLORS)[number];
+
+export const colorCode = (color: Color) => {
+  return COLORS.indexOf(color);
+};
