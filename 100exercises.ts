6903067622eb8dfeb500498dedf00682f3190f6c
Karim Chaya <chaya.karim.omar@gmail.com>
2024-11-22 14:05:41 -0600

    0.03

--- 100exercises.ts
+++ 100exercises.ts
@@ -27,3 +27,34 @@ export function decodedValue(colorsArr: ColorsArr) {
   value += COLORS.indexOf(colorsArr[1]);
   return Number(value);
 }
+
+// 3
+export const COLORS = [
+  "black", "brown",  "red", "orange",
+  "yellow", "green", "blue",
+  "violet", "grey", "white",
+] as const;
+
+type ColorsArr = typeof COLORS[number][];
+
+const formatOhms = (value: number): string => {
+  if (value < 1000) {
+    return `${value} ohms`;
+  } else if (value < 1000000) {
+    value /= 1000;
+    return `${value} kiloohms`;
+  } else if (value < 1000000000) {
+    value /= 1000000;
+    return `${value} megaohms`;
+  } else {
+    value /= 1000000000;
+    return `${value} gigaohms`
+  }
+}
+
+export function decodedResistorValue(colorsArr: ColorsArr) {
+  const baseValue = COLORS.indexOf(colorsArr[0]) * 10 + COLORS.indexOf(colorsArr[1]); 
+  const multiplier = COLORS.indexOf(colorsArr[2]);
+  const resistanceValue =  baseValue * Math.pow(10, multiplier);
+  return formatOhms(resistanceValue);
+}
