ce1afafb94b96440a500bf201fd4e6e6a28a224d
Karim Chaya <chaya.karim.omar@gmail.com>
2024-11-22 13:32:53 -0300

    0.02

--- 100exercises.ts
+++ 100exercises.ts
@@ -1,16 +1,8 @@
 // 1
-
 export const COLORS = [
-  "black",
-  "brown",
-  "red",
-  "orange",
-  "yellow",
-  "green",
-  "blue",
-  "violet",
-  "grey",
-  "white",
+  "black", "brown",  "red", "orange",
+  "yellow", "green", "blue",
+  "violet", "grey", "white",
 ] as const;
 
 type Color = (typeof COLORS)[number];
@@ -18,3 +10,20 @@ type Color = (typeof COLORS)[number];
 export const colorCode = (color: Color) => {
   return COLORS.indexOf(color);
 };
+
+
+// 2 
+export const COLORS = [
+  "black", "brown",  "red", "orange",
+  "yellow", "green", "blue",
+  "violet", "grey", "white",
+] as const;
+
+type ColorsArr = typeof COLORS[number][];
+
+export function decodedValue(colorsArr: ColorsArr) {
+  let value: string | number = "";
+  value += COLORS.indexOf(colorsArr[0]);
+  value += COLORS.indexOf(colorsArr[1]);
+  return Number(value);
+}
