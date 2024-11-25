63883909d20ca1fb01d35ab5b28923945075e7eb
Karim Chaya <chaya.karim.omar@gmail.com>
2024-11-25 17:10:38 -0600

    0.05

--- 100exercises.ts
+++ 100exercises.ts
@@ -87,3 +87,22 @@ export function toRna(sequence: Sequence): SequenceResultString {
 
   return resultSequence;
 }
+
+// 6
+
+const planets = {
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  earth: 1.0,
+  mars: 1.8808158,
+  jupiter: 11.862615, 
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132 
+} as const;
+
+type planet = keyof typeof planets;
+
+export function age(planet: planet, seconds: number): number {
+  return Math.round(seconds * 100 / (planets[planet] * 31557600)) / 100;
+}
